a280a4ac16a44af1a5c5a69fdb0e2a4998d76e62
Uladzislau Brel <vlad.brel.97@mail.ru>
2022-06-19 13:33:17 +0300

    Rename Mapper classes

--- src/main/java/by/brel/newsmanagement/mapper/NewsMapper.java
+++ src/main/java/by/brel/newsmanagement/mapper/NewsMapper.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MapperNews {
+public class NewsMapper {
 
     @Autowired
     private ModelMapper modelMapper;
